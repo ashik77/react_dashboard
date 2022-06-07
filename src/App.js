6c25d3120e7a059300feff43b7904d70ffab5130
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-06-07 15:23:26 +0300

    creating navbar and sidebar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import "./App.css";
 
 const App = () => {
+  const activeMenu = true;
+
   return (
     <React.Fragment>
       <BrowserRouter>
@@ -21,6 +23,25 @@ const App = () => {
               </button>
             </TooltipComponent>
           </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              Sidebar
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">Sidebar</div>
+          )}
+          <div
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? "md:ml-72" : "flex-2"
+            }`}
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              Navbar
+            </div>
+          </div>
+          <React.Fragment>
+            <Routes></Routes>
+          </React.Fragment>
         </div>
       </BrowserRouter>
     </React.Fragment>
